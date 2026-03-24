ba50d9a63b17f7bf6c97262bea84b99999c40ca9
dilmi kaushalya <kaushalyadilmi.av2003@gmail.com>
2026-03-24 12:48:15 +0530

    Add API Gateway with routing configuration

---
+++ Api gateway/src/main/java/com/assignment/apigateway/config/JwtUtil.java
@@ -0,0 +1,29 @@
+package lk.ijse.gdse72.apigateway.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+    public static final String SECRET = "MyCustomSecretKeyForAGMSProject12345678901234567890";
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+//    private Key getSignKey() {
+//        byte[] keyBytes = SECRET.getBytes(java.nio.charset.StandardCharsets.UTF_8);
+//        return io.jsonwebtoken.security.Keys.hmacShaKeyFor(keyBytes);
+//    }
+
+}
